dff52c7f1a4be5486d7fbe0e964b79804b28ba3d
João Ferreiro <ferreiro@pinkroom.dev>
2023-02-16 22:31:54 +0000

    fix bg color

--- App.tsx
+++ App.tsx
@@ -13,7 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333138',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },})
+  },
+})
